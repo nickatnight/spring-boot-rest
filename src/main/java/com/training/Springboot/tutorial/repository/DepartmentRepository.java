8ab6a2cc2262a34039df5e5cc9b9184a19ed2a46
nickatnight <nicholas.kelly@carta.com>
2022-02-24 09:35:56 -0800

    first commit

---
+++ src/main/java/com/training/Springboot/tutorial/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.training.Springboot.tutorial.repository;
+
+import com.training.Springboot.tutorial.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
---
+++ src/main/java/com/training/Springboot/tutorial/service/DepartmentService.java
@@ -0,0 +1,17 @@
+package com.training.Springboot.tutorial.service;
+
+import com.training.Springboot.tutorial.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+}
